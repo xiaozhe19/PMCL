2ae30858577941c9781fb118706b9b0e03bdd515
xiao_zhe19 <2694721821@qq.com>
2020-10-11 12:14:42 +0800

    beautify the main window

--- client/main.py
+++ client/main.py
@@ -1,20 +1,17 @@
 import sys
 from PyQt5.QtWidgets import QApplication ,QMainWindow
-from PyQt5.QtCore import Qt
 
-from ui import *
-
+from ui2 import *
+'''
 class MyMainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MyMainWindow,self)
         self.setupUi(self)
-
+'''
 if __name__ == "__main__":
   app = QtWidgets.QApplication(sys.argv) 
-  MainWindow = QtWidgets.QMainWindow()  
-  ui = Ui_MainWindow()          
-  ui.setupUi(MainWindow)         
-  MainWindow.show()            
+  ui = Ui_MainWindow()                
+  ui.show()            
   sys.exit(app.exec_()) 
   
        
